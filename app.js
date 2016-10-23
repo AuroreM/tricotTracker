22470d98a82aeea4a5bfd7f9fd9b54fa485b8bd3
louiszawadzki <louisz@theodo.fr>
2016-10-23 14:27:27 +0200

    Start back

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-First commit
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+
+io.on('connection', function (socket) {
+  var moveCount = 0;
+  app.post('/move', function (req, res, next) {
+    moveCount++;
+    socket.emit('move', {moveCount: moveCount});
+    res.send(200);
+  })
+});
